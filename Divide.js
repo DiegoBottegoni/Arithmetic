d9799a44b4eac4bfd496ed0ae5e0a7d8a1799e99
Diego Bottegoni <146596468+DiegoBottegoni@users.noreply.github.com>
2024-04-05 13:46:54 -0300

    Divide.js

--- Divide.js
+++ Divide.js
@@ -15,29 +15,15 @@ function stringToNumber(str) {
     const num1 = this;
     const num2 = string;
     
-    const dividend = stringToNumber(num1);
-    const divisor = stringToNumber(num2);
+    const dividend = BigInt(num1);
+    const divisor = BigInt(num2);
   
     if (divisor === 0) {
       throw new Error("Division by zero.");
     }
   
-    let quotientSign = 1;
-    if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
-      quotientSign = -1;
-    }
-  
-    let absDividend = dividend < 0 ? -dividend : dividend;
-    const absDivisor = divisor < 0 ? -divisor : divisor;
-  
-    let quotient = 0;
-    while (absDividend >= absDivisor) {
-      absDividend -= absDivisor;
-      quotient++;
-    }
-  
-    quotient *= quotientSign;
-  
+    const quotient = dividend / divisor;
+
     return quotient.toString();
   };
   
@@ -48,10 +34,10 @@ function stringToNumber(str) {
     ["12345", "123", "100"],
     ["999", "11", "90"],
     ["0", "1", "0"],
+    ["4444444444444444444444444444444440000000000000000000000000000000", "2", "2222222222222222222222222222222220000000000000000000000000000000"],
   ];
   
   for (const testCase of testCases) {
     const result = testCase[0].divide(testCase[1]);
     console.log(`Expected: ${testCase[2]}, Actual: ${result}`);
   }
-  
